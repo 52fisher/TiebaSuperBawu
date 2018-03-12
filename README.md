631e1b8d7e008c7b06a8a9a57b7c5de8ca9235f6
fisher <tiebayu@126.com>
2018-03-12 21:11:25 +0800

    修复更新问题

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # TiebaSuperBawu
 吧务功能增强：贴内增加用户拉黑按钮（仅大吧主）、吧务后台增加多选框实现批量处理（批量封禁拉黑）、自动拒绝被删帖或封禁吧友申诉、贴吧神兽时长自定义、贴内显示真实ID等.
 
+## 安装地址
+[点我安装](https://gitee.com/fisher52/TiebaSuperBawu/raw/master/SuperBawu.user.js)
+
 ## 参数说明
 注意：如果不需要其中部分功能,请直接注释掉相关语句.
 ```javascript
--- SuperBawu.user.js
+++ SuperBawu.user.js
@@ -7,9 +7,9 @@
 // @exclude     http://tieba.baidu.com/bawu2/platform/listBlackUser*
 // @exclude     http://tieba.baidu.com/bawu2/platform/listBawuDel*
 // @exclude     http://tieba.baidu.com/bawu2/postaudit/*
-// @updateURL    https://gitee.com/fisher52/tbMerge/raw/master/js/tbMerge.meta.js
-// @downloadURL    https://gitee.com/fisher52/tbMerge/raw/master/js/tbMerge.user.js
-// @version     1.7
+// @updateURL   https://gitee.com/fisher52/TiebaSuperBawu/raw/master/superBawu.meta.js
+// @downloadURL https://gitee.com/fisher52/TiebaSuperBawu/raw/master/superBawu.user.js
+// @version     1.8
 // @author    52fisher
 // @description  贴吧吧务功能增强
 // @grant       none
